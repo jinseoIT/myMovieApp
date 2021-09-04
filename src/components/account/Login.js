0d6cc75bc9984d1a93cce259bfa2de84e7793fd7
jisneoit <jinseoit@gmail.com>
2021-09-04 23:23:59 +0900

    로그인 수정

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -7,12 +7,33 @@ function Login() {
   const [userInfo, setUserInfo] = useState({email:'', password:''});
 
   const handleChange = (e) => {
-    const { name , value } = e.target;
+    const { name, value } = e.target;
     setUserInfo({ ...userInfo, [name]: value });
   }
   
   const handleSubmit = async (e) => {
+    e.preventDefault();
     console.log(userInfo.email, userInfo.password);
+    const endPoint = 'http://localhost:5001/api/user/login';
+    axios({
+      url: endPoint,
+      method: 'post',
+      data: {
+        email: userInfo.email,
+        password: userInfo.password
+      }
+    })
+    .then(res => {
+      console.log(res);
+      if (!res.data.loginSuccess) {
+        alert(res.data.message);
+      } else {
+        alert('로그인 성공');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
   
   return (
@@ -22,19 +43,21 @@ function Login() {
           <form onSubmit={handleSubmit}>
             <div className="int-area">
             <input
-              type="email"
+              type="text"
               id="email"
+              name="email"
               autoComplete="off"
               required
               value={userInfo.email}
               onChange={handleChange}
-            />`
+            />
               <label htmlFor="email">USER EMAIL</label>          
             </div>
             <div className="int-area">
             <input
               type="password"
               id="pw"
+              name="password"
               autoComplete="off"
               required
               value={userInfo.password}
